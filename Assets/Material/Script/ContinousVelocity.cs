a0388ab7fdf0ea6bfaa5fb6eac392d813e83d32e
GustavoHenriqueXavierdeBrito <67766345+GustavoHenriqueXavierdeBrito@users.noreply.github.com>
2020-07-02 22:17:21 -0300

    Add files via upload

---
+++ Assets/Material/Script/ContinousVelocity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinousVelocity : MonoBehaviour
+{
+
+    public Vector2 velocity;
+
+    void Start() {
+        GetComponent<Rigidbody2D> ().velocity = velocity;
+    }
+}
---
+++ Assets/Material/Script/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+        public GameObject ship;
+        public float interval = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("NextShip", interval, interval);
+    }
+
+    // Update is called once per frame
+    void NextShip()
+    {
+        Instantiate (ship, transform.position, Quaternion.identity);
+    }
+}
